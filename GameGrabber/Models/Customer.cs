55eb8fd162d0ed8a4099ef16e4776e8734c7c1ab
Brooklyn Eti <ac106494@avcol.school.nz>
2021-03-18 12:30:01 +1300

    CreatPage for customers

--- GameGrabber/Models/Customer.cs
+++ GameGrabber/Models/Customer.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameGrabber.Models
 {
     public class Customer
     {
+        [Required]
         public int CustomerID { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string FirstMidName { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public int PhoneNumber { get; set; }
-        public Product Product { get; set; }
+        [Required]
+         public Product Product { get; set; }
+        
 
 
 
